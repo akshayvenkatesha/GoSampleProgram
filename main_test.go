76fd37d7542ea0b7349db0fe9d6434277dfbcf0a
Akshay N V <akshayvenkatesha@gmail.com>
2018-01-07 00:46:20 +0530

    added TestInitializePlayers test case in main_test.go

--- main_test.go
+++ main_test.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"Business-House-Game/constants"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestInitializePlayers(t *testing.T) {
+	numberOfPlayers := 3
+	players := InitializePlayers(numberOfPlayers)
+
+	for i, player := range players {
+		if !strings.EqualFold(player.PlayerName, "player-"+strconv.Itoa(i+1)) {
+			t.Errorf("Expected name %s but found %s", player.PlayerName, "player-"+strconv.Itoa(i))
+		}
+
+		if player.NetWorth != constants.InitialMoney {
+			t.Errorf("Expected NetWorth %d but found %d", player.NetWorth, constants.InitialMoney)
+		}
+	}
+
+}
